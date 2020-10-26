112ed2511ee09ff152151c2d02d9bafa9fbf52a7
jpzanchin <jpzanchin@gmail.com>
2020-10-25 22:47:34 -0300

    Commit inicial

---
+++ src/main/java/com/zanchin/modelo/SpringModelApplication.java
@@ -0,0 +1,13 @@
+package com.zanchin.modelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanchin/modelo/SpringModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanchin.modelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
